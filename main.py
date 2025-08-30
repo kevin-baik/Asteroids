11d5de8fcc027d3f21bc9961ebf98fd03511bc1c
kbaik <>
2025-08-29 17:59:17 -0700

    create CircleShape class

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    game_clock = pygame.time.Clock()
+    clock = pygame.time.Clock()
     dt = 0
 
     while True:
@@ -15,7 +15,7 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
-        dt = game_clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
