7792955ed4a929bd895e139ed96d8e83a3ebac4e
kbaik <>
2025-08-29 20:57:11 -0700

    add player directional movements

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")
         player.update(dt)
-        player.draw(screen)
 
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 
